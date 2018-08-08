aad6563481b028233f7ce26fd85c36f2f15ac722
nandakumarp <nandakumar1990@gmail.com>
2018-08-09 00:13:26 +0530

    Added projects page and modified header

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects', {
+        pageTitle: 'My projects list'
+    })
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to process request'
